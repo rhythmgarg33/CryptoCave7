bd582e629e8c42542fce1e2d3c49276494fde15d
Rhythm Garg <rgarg6_be19@thapar.edu>
2022-07-20 16:44:43 +0530

    Update Login.js

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -8,33 +8,15 @@ const Login = ({ handleClose }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const { setAlert } = CryptoState();
-
   const handleSubmit = async () => {
     if (!email || !password) {
-      setAlert({
-        open: true,
-        message: "Please fill all the Fields",
-        type: "error",
-      });
       return;
     }
 
     try {
       const result = await signInWithEmailAndPassword(auth, email, password);
-      setAlert({
-        open: true,
-        message: `Sign Up Successful. Welcome ${result.user.email}`,
-        type: "success",
-      });
-
       handleClose();
     } catch (error) {
-      setAlert({
-        open: true,
-        message: error.message,
-        type: "error",
-      });
       return;
     }
   };
